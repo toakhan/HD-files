5be5623a9617203a8c77e293b4bbc7e7197953dc
Amjad Khan <[toakhan@hotmail.com]>
2022-12-17 13:25:14 +0000

    added code to index.html & fortuneTeller.js

---
+++ T03-variables-datatypes/fortuneTeller.js
@@ -0,0 +1,12 @@
+T03 - Compulsory Task 2: Fortune Teller
+
+let motherFirstName=prompt("What is your mother's first name?");
+console.log(motherFirstName);
+let streetName= prompt("Please tell the name of the street you grew up on?");
+console.log(streetName);
+let favColor=prompt("What is your favorite color?");
+console.log(favColor);
+let currentAge=prompt("What is your current age?");
+console.log(currentAge);
+let aNumber=prompt("Please choose a number between 1 and 10?");
+console.log(aNumber);
