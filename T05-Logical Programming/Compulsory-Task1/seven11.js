8226e7c4938f13c554360eed40aa60fe0906cc2f
Amjad Khan <[toakhan@hotmail.com]>
2022-12-24 11:51:08 +0000

    added code to task 1

--- T05-Logical Programming/Compulsory-Task1/seven11.js
+++ T05-Logical Programming/Compulsory-Task1/seven11.js
@@ -1,4 +1,30 @@
 T05-Compulsory Task 1
 
-let inputNum=prompt("Input a number : " + inputNum);
-    if inputNum%7=0
+let num=prompt("Please enter a number : ");
+
+    if ((num % 7===0) && (num%11===0)) {
+        console.log("Number is divisible by both 7 and 11 with remainder 0");
+
+}else{
+        console.log("Number is not divisible by both 7 and 11");
+}
+    
+    if (num % 11===0) {
+        console.log("Number is divisible by 11");
+}else{
+    console.log("Number is not divisible by 11");
+    }
+
+    if (num % 7===0) {
+        console.log("Number is divisible by 7");
+}else{
+    console.log("Number is not divisible by 7");
+    }
+
+
+    if ((!num % 7===0) && (!num % 11===0)) {
+        console.log("Number is neither divisible by 7 nor by 11");
+
+}else{
+        console.log("Number is either divisible by 7 or by 11");
+}
