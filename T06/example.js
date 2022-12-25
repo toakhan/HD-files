5f7d151ef799a14da3d71c9f6dbc99c236e4b877
toakhan <116645794+toakhan@users.noreply.github.com>
2022-12-25 17:46:36 +0000

    Add files via upload

---
+++ T06/example.js
@@ -0,0 +1,17 @@
+//************* HELP *****************
+//REMEMBER THAT IF YOU NEED SUPPORT ON ANY ASPECT OF YOUR COURSE SIMPLY LOG IN TO www.hyperiondev.com/support TO: 
+//START A CHAT WITH YOUR MENTOR, SCHEDULE A CALL OR GET SUPPORT OVER EMAIL.
+//************************************
+
+
+// PLEASE ENSURE YOU OPEN THIS FILE IN VS Code otherwise you will not be able to read it.
+
+
+// *** NOTE ON COMMENTS ***
+// This is a comment in JavaScript. 
+// Comments can be placed anywhere in JavaScript code and the computer ignores them -- they are intended to be read by humans. 
+// Any line with a // in front of it is a comment.
+/* If you want to add a long comment that spans across several lines,
+then you would use a multi-line comment. As you can see, a multi-line
+comment starts with a /* and ends with */
+// Please read all the comments in this example file and all others.
