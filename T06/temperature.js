7b8c4840b5eba974b53ed6d2c71334ccaff078af
Amjad Khan <[toakhan@hotmail.com]>
2022-12-26 20:37:12 +0000

    added code to temperature.js file

--- T06/temperature.js
+++ T06/temperature.js
@@ -1,1 +1,15 @@
 
+
+
+
+
+let convFrom=prompt("In which metric is the temperature, you are converting? Input C for Celsius, F for Fahrenheit or K for Kelvin: " );
+console.log("You would like to convert temperature from : " + convFrom);
+
+let inputNum= prompt("Please enter the temperature in degrees : ");
+//console.log(`You would like to convert: ${inputNum}${convFrom}`);
+console.log("You would like to convert: " + inputNum + convFrom );
+
+let convTo= prompt("To which metric would you like to convert the temperature? Input C for Celsius, F for Fahrenheit or K for Kelvin: ");
+console.log(`You would like temperature conversion from: ${inputNum}${convFrom} to ${convTo}`)
+
