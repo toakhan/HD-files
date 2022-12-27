200017a7a4bd7cde204796570f216dd73bf9f94d
Amjad Khan <[toakhan@hotmail.com]>
2022-12-27 09:40:04 +0000

    improved code in quadrilaterals.js

--- T05-Logical Programming/Compulsory-Task2/quadrilaterals.js
+++ T05-Logical Programming/Compulsory-Task2/quadrilaterals.js
@@ -1,9 +1,9 @@
 // T05 - Compulsory Task 2
 
-let side1 = prompt ("Enter length of side 1: " );
-let side2 = prompt ("Enter length of side 2: " );
-let side3 = prompt ("Enter length of side 3: " );
-let side4 = prompt ("Enter length of side 4: " );
+let side1 = parseFloat(prompt ("Enter length of side 1: " ));
+let side2 = parseFloat(prompt ("Enter length of side 2: " ));
+let side3 = parseFloat(prompt ("Enter length of side 3: " ));
+let side4 = parseFloat(prompt ("Enter length of side 4: " ));
 
 // Opposite corners (angles) are equal in all
 // four quadrilateral shapes. 
@@ -14,30 +14,30 @@ let corner3 =  90;
 let corner4 =  90;
 
 
-if ((side1===side2===side3===side4)) 
-     && ((corner1===corner2===corner3===corner4)){
+if ((side1===side2)&&(side3===side4)
+     && (corner1===corner2)&&(corner3===corner4)){
 
         console.log ("It is a square.");
 
     }else if
 
-        ((side1===side2===side3===side4)) 
-        && ((corner1===corner3)<90 || (corner2===corner4)<90))
-        &&  (((corner1===corner3)>90 || (corner2===corner4)>90)){
+        (((side1===side2)&&(side3===side4)
+        && (corner1===corner3)<90 || (corner2===corner4)<90)
+        &&  ((corner1===corner3)>90 || (corner2===corner4)>90)){
 
             console.log ("It is a rhombus."); 
 
     }else if
 
-         ((side1===side3) && (side2===side4)) 
-        && ((corner1===corner2===corner3===corner4)){
+         ((side1===side3) && (side2===side4) 
+        && (corner1===corner2)&&(corner3===corner4)){
 
             console.log ("It is a rectangle.");
 
     }else if
 
-        ((side1===side3) && (side2===side4)) 
-        && ((corner1===corner3) && (corner2===corner4)){
+        ((side1===side3) && (side2===side4) 
+        && (corner1===corner3) && (corner2===corner4)){
 
         console.log ("It is a parallelogram.");
 
--- T06/temperature.js
+++ T06/temperature.js
@@ -1,7 +1,7 @@
 
-
-
-
+let C="C";
+let F="F";
+let K="K";
 
 let convFrom=prompt("In which metric is the temperature, you are converting? Input C for Celsius, F for Fahrenheit or K for Kelvin: " );
 console.log("You would like to convert temperature from : " + convFrom);
@@ -13,3 +13,15 @@ console.log("You would like to convert: " + inputNum + convFrom );
 let convTo= prompt("To which metric would you like to convert the temperature? Input C for Celsius, F for Fahrenheit or K for Kelvin: ");
 console.log(`You would like temperature conversion from: ${inputNum}${convFrom} to ${convTo}`)
 
+let toCfromF = (F-32)*5/9;
+let toCfromK = K-273.15;
+let toFfromC = C*9/5+32;
+let toFfromK = K*9/5-459.67;
+let toKfromC = C+273.5;
+let toKfromF = (F+459.67)*5/9;
+
+if ((convFrom==C)&&(convTo==F)) {
+   console.log(`The temperature conversion is (${parseFloat(inputNum) * 9/5 + 32})   °F`)
+
+
+}
