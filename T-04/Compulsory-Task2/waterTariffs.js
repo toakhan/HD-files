007d6a082002cb456ecf562e83c3f4b9eddbbd8a
Amjad Khan <[toakhan@hotmail.com]>
2022-12-29 13:43:38 +0000

    added code to tariffs.js file

--- T-04/Compulsory-Task2/waterTariffs.js
+++ T-04/Compulsory-Task2/waterTariffs.js
@@ -2,7 +2,7 @@
 
 
 
-let waterUsed = prompt("Enter the volume of water used in litres : ");
+let waterUsed = parseInt (prompt("Enter the volume of water used in litres : "));
 console.log("Water used in litres : " + waterUsed);
 
 value=0;
@@ -10,19 +10,26 @@ waterUsed=parseInt(waterUsed);
 waterUsed=waterUsed/1000;
 
 if (waterUsed <= 6) {
-    console.log("Your water bill in Rands is : " + waterUsed*15.73);
+    console.log ("Your water bill in Rands is : " + waterUsed*15.73);
 }
+
 else if (waterUsed > 6 && waterUsed<= 10.5){
 value=6*15.73
-    console.log("Your water bill in Rands is : " + ((waterUsed-6)*22.38)+value); 
+waterUsed=parseInt(waterUsed);
+
+    console.log ("Your water bill in Rands is : ") +(waterUsed-6)*22.38 + value;
 }
 
 else if (waterUsed > 10.5 && waterUsed<= 35){
     value=6*15.73
+    waterUsed=parseInt(waterUsed);
+    waterUsed=waterUsed/1000;
     console.log("Your water bill in Rands is : ") + (waterUsed-6)*31.77 +value;
 }
 else if (waterUsed > 35){
     value=6*15.73
+    waterUsed=parseInt(waterUsed);
+    waterUsed=waterUsed/1000;
     console.log("Your water bill in Rands is : ") + ((waterUsed-6)*69.76)+value; 
 }
 else {
