3a07d520ba5181262a4d1dddba523463af4d9b19
Amjad Khan <[toakhan@hotmail.com]>
2022-12-30 18:19:15 +0000

    added code to quiz.js

---
+++ T08-While Loops/Compulsory-Task-3/quiz.js
@@ -0,0 +1,78 @@
+// T-08 - Compulsory Task 3
+
+/*let correctAns = "c";
+let a = "a";
+let b = "b";
+let c = "c";
+let d = "d";
+let count = 0;
+
+do {
+    prompt("What color is the Sky? \na: Purple \nb: Pink \nc: Blue \nd: Yellow. Enter a, b, c or d : ");
+    
+    count= count+1;
+ while (correctAns != "c");
+    console.log("That's incorrect!.")}
+   
+    let retry = alert("Try again? y/n")
+
+if (retry == y) {
+    count ++;
+}
+else if (retry == n) {
+    break [label];
+}
+else (answer === c) {
+    console.log("That's correct !");
+}
+
+
+
+
+let inputCol=prompt("What color is the Sky? \na: Purple \nb: Pink \nc: Blue \nd: Yellow. Enter a, b, c or d : ");
+console.log(`You have entered ${inputCol}.`);
+
+let c = "c"
+let y = Boolean (true);
+let n = Boolean (false);
+
+if (inputCol !== c){
+console.log ("Answer is incorrect. Try again?");
+let yesNo = prompt("To try again, enter 'y' for yes or 'n' for no : ");
+}else if (yesNo == y){
+//let reTry = prompt("What color is the Sky? \na: Purple \nb: Pink \nc: Blue \nd: Yellow. Enter a, b, c or d : ");
+
+}else if (yesNo == n){
+    console.log ("Thank you for taking the quiz.");
+
+}else
+console.log ("Answer is correct.");
+
+*/
+
+let i = 0;
+while (i < 10) {
+    console.log("The number is " + i);
+    if (i < 9) {
+        console.log("The value for i is now: " + i);
+        console.log("The condition (i < 10) is, therefore, still TRUE so this loop will run again!");
+    } else {
+        console.log("The value for i is now: " + i);
+        console.log("The condition (i < 10) will be FALSE with the next test so this loop will now terminate!!");
+        console.log("GOODBYE");
+    }
+    i++;
+}
+console.log();
+
+
+
+
+    
+
+
+
+
+
+
+
