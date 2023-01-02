76cb6904472c53ffbda36e21b30b316f0e8f7a6a
Amjad Khan <[toakhan@hotmail.com]>
2023-01-02 10:22:24 +0000

    added correct link in html file to js file

--- T08-While Loops/Compulsory-Task-3/quiz.js
+++ T08-While Loops/Compulsory-Task-3/quiz.js
@@ -49,8 +49,8 @@ let yesNo = prompt("To try again, enter 'y' for yes or 'n' for no : ");
 console.log ("Answer is correct.");
 
 */
-
-let i = 0;
+//from example(3).js to run
+/*let i = 0;
 while (i < 10) {
     console.log("The number is " + i);
     if (i < 9) {
@@ -64,7 +64,86 @@ while (i < 10) {
     i++;
 }
 console.log();
+*/
+// ============ Example 2 ============
+console.log("Example of a do while loop:");
+let j = 0;
 
+do {
+    console.log("The number is " + j);
+    j++;
+} while (j < 10);
+console.log();
+// ============ For loop ============
+// This loop will keep repeating for a predetermined number of times. 
+
+// ============ Example 3 ============
+/*
+console.log("Example of a for loop 'for (l = 0; l < 5; l++)':");
+for (let l = 0; l < 5; l++) {
+    console.log("The number is " + l);
+}
+console.log();
+//=============Nested Loop =====================
+/*The following code will output a rectangle of *s.
+The dimensions of the rectangle are determined by the variables
+numRows (which stores the number of rows that will be printed) and 
+numColumns (which stores the number of columns that will be printed) */
+
+// ============ Example 4 ============
+let numRows = 5;
+let numColumns = 10;
+let strRowOutput = "";
+
+for (let row = 1; row <= numRows; row++) {
+    for (let column = 1; column <= numColumns; column++) {
+        strRowOutput += "*";
+    }
+    console.log(strRowOutput);
+    strRowOutput = "";
+}
+console.log();
+
+/*The following code will output a rectangle of *s similar to the one created above
+BUT instead of having a solid pattern printed, we want to only print the border of the 
+rectangle. E.g. output should be 
+**********
+*        *
+*        *
+*        *
+********** 
+
+instead of 
+
+**********
+**********
+**********
+**********
+**********
+
+*/
+/*
+for (let row = 1; row <= numRows; row++) {
+    for (let column = 1; column <= numColumns; column++) {
+        if (
+            row === 1 ||
+            row === numRows ||
+            column === 1 ||
+            column === numColumns
+            ) {
+            //Notice how we make use of the OR logical operator here.
+            strRowOutput += "*";
+        } 
+        else {
+            strRowOutput += " ";
+        }
+    }
+    console.log(strRowOutput);
+    strRowOutput = "";
+}
+
+*/
+// ****************** END OF EXAMPLE CODE ********************* 
 
 
 
