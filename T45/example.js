5502aad49d72e8d31f1ec8ddf618dc0b355c542a
toakhan <116645794+toakhan@users.noreply.github.com>
2023-01-14 10:19:36 +0000

    Add files via upload

---
+++ T45/example.js
@@ -0,0 +1,88 @@
+//************* HELP *****************
+//REMEMBER THAT IF YOU NEED SUPPORT ON ANY ASPECT OF YOUR COURSE SIMPLY LOG IN TO www.hyperiondev.com/support TO: 
+//START A CHAT WITH YOUR MENTOR, SCHEDULE A CALL OR GET SUPPORT OVER EMAIL.
+//************************************
+
+
+// PLEASE ENSURE YOU OPEN THIS FILE IN IDLE otherwise you will not be able to read it.
+
+// *** NOTE ON COMMENTS ***
+// This is a comment in Javascript. 
+// Comments can be placed anywhere in Javascript code and the computer ignores them -- they are intended to be read by humans. 
+// Any line with a // in front of it is a comment.
+// Please read all the comments in this example file and all others.
+
+
+// ========= Reading Javascript code ========= 
+// You're actually reading a Javascript program right now.
+// Comments in Javascript appear in GREEN if you have the file opened in Notepad++ and RED if you have it opened in IDLE.
+// Keywords of the Javascript language appear in BLUE in Notepad++ and PURPLE in IDLE. 
+// Most other programming languages have the same structure as Javascript, so if you learn Javascript, you can learn the others more easily! It's not like learning human languages.
+
+
+//  =========  The console.log() function ========= 
+// You may want your program to display or output information to the user.
+// The most common way to view program output is to use the print function.
+// To use print, we enter the print command followed by one or more arguments. 
+
+// ************ Example 1 ************
+console.log("Hello, World!")
+// When you run this program the computer will output the argument Hello, World!
+// Note that the argument is enclosed in double quotes ("..."), which are not displayed in the output.
+// This is because "Hello, World!" is a string or list of characters.
+
+// ************ Example 2 ************
+console.log("Printing a string.") 
+
+// A common SYNTAX error you could make above is by forgetting to add a closing ".
+// Remember that all opening quotation marks (") require a closing one!
+
+
+// ************ Example 3 ************
+
+let name = "HyperionDev"
+let age = 10
+
+// The variable 'name' stores "Hello" into the box as a string.
+// The variable 'age' also stores 10 into the box as a numerical value.
+// Now, when you try running this file, notice the output produced by the following command.
+console.log(name, age)
+
+// A common SYNTAX error you could make above is by forgetting to add a closing ‘)’.
+// Remember that all opening brackets ‘(’ , require a matching closing one!
+
+
+//  ========= Running Javascript files ========= 
+// Now that you know how to write code, it's time to learn how to execute your code to see what the output is.
+// Let's 'RUN' this Javascript file and take a look at what output it produces (if any).
+// When you write Javascript code, you'll have to run it often to test that your programs are doing what you'd like them to do.
+
+// There are different ways to 'run' Javascript files.
+
+//  ========= OPTION 1: Run from IDLE Javascript GUI -- THE EASY WAY ========= 
+// The easiest way to run Javascript files and this program is through a GUI (Graphical User Interface). 
+// Press F5 on your keyboard to RUN the Javascript file and the output will appear in a separate Javascript output (Javascript Shell) window!
+// You can use this method to run ANY Javascript file (file with a .js extension). 
+// If there is an error in your code, the code won't run and the error will be printed out in the Javascript Shell window.
+
+// Errors are things like trying to add two variables that aren't the same data type, or using a variable that isn't declared! 
+// (i.e. if you say num = num1+num2 and you haven't said what num1 and num2 are above this statement!)
+// We'll talk about errors more later.
+
+// Play around with this program. Change statements and delete things, run the code and see what happens.
+
+// We advise that you complete all tasks and open all example files in IDLE from now on.
+// Perhaps create a shortcut to IDLE on your desktop so that you can access it faster.
+// You can also use Notepad++ to view the example.js files, but Notepad++ can only be used to view the text of a code. You can't run programs from within Notepad++.
+
+
+//  ========= OPTION 2: Use any GUI/IDE that you want.  -- ADVANCED ========= 
+// An IDE is a program like the IDLE Javascript GUI.
+// You get many programs that let you run code within them.
+// Some of these IDEs are more complicated than others.
+// As long as you know how to use IDLE, you can complete our entire course.
+
+
+// ****************** END OF EXAMPLE CODE ********************* // 
+
+
