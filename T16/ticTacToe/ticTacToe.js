9745bdd30b6c847c50786e788718114215f2dbe3
toakhan <116645794+toakhan@users.noreply.github.com>
2023-01-14 08:14:55 +0000

    Add files via upload

---
+++ T16/ticTacToe/ticTacToe.js
@@ -0,0 +1,36 @@
+// Define arrays
+const rowWin = [
+    ["O", "O", "O"],
+    ["", "", ""],
+    ["", "", ""]
+]
+
+const colWin = [
+    ["", "X", ""],
+    ["", "X", ""],
+    ["", "X", ""]
+]
+
+const diagonalWin = [
+    ["", "", "O"],
+    ["", "O", ""],
+    ["O", "", ""]
+]
+
+const diagonalWinInverse = [
+    ["X", "", ""],
+    ["", "X", ""],
+    ["", "", "X"]
+]
+
+// evaluatePlay function.
+// Within this function, write the code to evaluate weather a winning play has been made or not
+// Your program must be able to evaluate all the grids defined above.
+// The function should inform the user of which sign has  won and which sign has lost
+// You may add additional parameters to assist you
+function evaluatePlay(grid){
+
+    // ------------------------------- Have -------------------------------------------
+
+    // ------------------------------- Fun  -------------------------------------------
+}
