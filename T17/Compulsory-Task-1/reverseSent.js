1663d2dc0f6df8204a606457d80e22282751e729
Amjad Khan <[toakhan@hotmail.com]>
2023-01-28 22:29:12 +0000

    added code to both tasks

--- T17/Compulsory-Task-1/reverseSent.js
+++ T17/Compulsory-Task-1/reverseSent.js
@@ -1,7 +1,17 @@
 
-/*
-Reverse a String With Recursion.
-For this solution, we will use two methods: 
+function reverseString(str) {
+    if (str === "") // This is the terminal case that will end the recursion
+      return "";
+    
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+}
+reverseString("hello");
+
+/* Comments on above code are made as follows:
+To Reverse a String With Recursion.
+
+Two methods are used: 
 the String.prototype.substr() method and 
 the String.prototype.charAt() method.
 
@@ -15,27 +25,14 @@ of characters.
 The charAt() method returns the specified 
 character from a string.
 "hello".charAt(0); // "h"
+
 The depth of the recursion is equal to the 
 length of the String. This solution is not 
 the best one and will be really slow if 
-the String is very long and the stack size 
-is of major concern.
-*/
+the String is very long.
 
-function reverseString(str) {
-    if (str === "") // This is the terminal case that will end the recursion
-      return "";
-    
-    else
-      return reverseString(str.substr(1)) + str.charAt(0);
-}
-reverseString("hello");
-
- /*    
-  First Part of the recursion method
-  You need to remember that you won’t have 
-  just one call, you’ll have several 
-  nested calls
+First Part of the recursion method
+involves several nested calls.
   
   Each call: str === "?"        	                
   reverseString(str.subst(1)) + str.charAt(0)
@@ -50,8 +47,8 @@ reverseString("hello");
   5th call – reverseString("o")       
   will return   reverseString("")     + "o"
   
-  Second part of the recursion method
-  The method hits the if condition and 
+  Second part of the recursion method:
+  The method hits, the if condition and 
   the most highly nested call returns 
   immediately
   
@@ -62,17 +59,9 @@ reverseString("hello");
   1st call will return reverserString("ello") + "h" = "o" + "l" + "l" + "e" + "h" 
   */
   
-  //Without comments:
-/*
-  function reverseString(str) {
-    if (str === "")
-      return "";
-    else
-      return reverseString(str.substr(1)) + str.charAt(0);
-  }
-  reverseString("hello");
 
-*/
+
+
 //Conditional (Ternary) Operator:
 /*
 function reverseString(str) {
--- T17/Compulsory-Task-2/searchIndex.js
+++ T17/Compulsory-Task-2/searchIndex.js
@@ -1,68 +1,54 @@
-/*
-If you need the index of the found element 
-in the array, use findIndex().
+
+function dataArray( arr ){
+    for ( let i = 0; i < arr.length; i++ ) {
+	
+        if( arr[i] == "cat" ){
+            console.log( "cat found.");
+            return true;
+        } else if( Array.isArray( arr[i] ) ){
+            if (dataArray(arr[i])) return true;
+        }
+    }
+
+    return false;
+}
+
+let arr=[["java", "html","javaScripts", "css"]];
+
+console.log(dataArray(arr));
+console.log(arr.findIndex(dataArray));
+
+
+
+/*	Comments:
+To find index of an element in an array 
+findIndex() is used.
  
-If you need to find the index of a value, 
+To find the index of a value, 
 use indexOf() . ...
 
-If you need to find if a value exists in an 
+To find if a value exists in an 
 array, use includes() . ...
 
-If you need to find if any element satisfies 
+To find if any element satisfies 
 the provided testing function, use some() .
 
 */
 
-//I'm trying to select Nth element from 
-//array using this function:
-
-function nthArr(arr, index)
-    if (index === 0){
-        return arr[index];
-else{
-        return nthArr(arr.slice(1), --index);
-
-}
-    }
-nthArr([1,2,3,4,5,6],3)
-
-//I would await that it returns 4, but instead 
-//I get 'undefined'.
-//How should I return correct value?
-
-
-if (index === 0)
-    return arr[index];
-else {
-    return nthArr(arr.slice(1), --index); // Note the return
-}
-
-//You are missing a return statement in 
-//the else branch.
-
-if (index === 0)
-    return arr[index];
-else {
-    return nthArr(arr.slice(1), --index); // Note the return
-}
-
-
 /*
-* Approach : The idea is to compare x 
-with the last element in arr[]. 
-If the element is found at the last position,
-return it. Else recur searchElement() for 
-remaining array and element x.
-*/
-
-
-#include <iostream>
-
-using namespace std;
-
+var getSubMenuItem = function (subMenuItems, id) {
+    if (subMenuItems) {
+        for (var i = 0; i < subMenuItems.length; i++) {
+            if (subMenuItems[i].id == id) {
+                return subMenuItems[i];
+            }
+            var found = getSubMenuItem(subMenuItems[i].items, id);
+            if (found) return found;
+        }
+    }
+};
 // Recursive function to search for x in arr[]
 int searchElement(int arr[], int size, int x) {
-	
 	size--;
 	
 	// Base case (Element not present in the array)
@@ -79,7 +65,6 @@ int searchElement(int arr[], int size, int x) {
 
 }
 
-
 // Driver code
 int main() {
 	int arr[] = {17, 15, 11, 8, 13, 19};
@@ -92,5 +77,12 @@ int main() {
 		cout << "Element " << x << " is not present in the array";
 	return 0;
 }
+*/
 
-// Code contributed by farzams101
+
+
+
+
+    
+
+    
