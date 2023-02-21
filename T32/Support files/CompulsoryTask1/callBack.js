04f2fccc7cb08ea8c852b971067211ce855ab50b
Amjad Khan <[toakhan@hotmail.com]>
2023-02-21 05:35:49 +0000

    added code to .js file

--- T32/Support files/CompulsoryTask1/callBack.js
+++ T32/Support files/CompulsoryTask1/callBack.js
@@ -1,5 +1,34 @@
-// Code here for Compulsory Task 1
+// Code for Compulsory Task 1:
 
+// setting variables to hold the intervalID
+// and also a counter
+let intervalID;
+let counter = 0;
+
+let startCounter = function() {
+  intervalID = setInterval(() => {
+    // increment the counter
+    counter++;
+    // log the counter to the console
+    console.log(counter);
+  }, 1000);
+}
+
+function stopCounter() {
+  clearInterval(intervalID);
+}
+
+let startButton = document.getElementById('start');
+let stopButton = document.getElementById('stop');
+
+// add the startCounter function as the callback for the start button click event
+startButton.addEventListener("click", startCounter);
+
+// add the stopCounter function as the callback for the stop button click event
+stopButton.addEventListener("click", stopCounter);
+
+
+/*
 // setting variables to hold the intervalID
 // and also a counter
 let intervalID;
@@ -28,3 +57,4 @@ startButton.addEventListener("click", );
 // this button should the counter from outputting to the console
 stopButton.addEventListener("click", );
 
+*/
